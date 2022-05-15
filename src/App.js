4a25551814e760f2424e412fa164ddd9f37273f1
m.tighzaoui <tighzaoui@outlook.com>
2022-05-16 00:19:08 +0100

    add some pages to test if the links on the navbar is working or not

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { Container } from "./app.style";
 import HeadSection from "./components/HeadSection";
+import HomePage from "./components/Home_Page/Home";
+import ShopPage from "./components/Shop_Page/Shop";
+import ContactPage from "./components/Contact_Page/Contact";
+import AboutPage from "./components/About_Page/About";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,10 +12,10 @@ function App() {
       <Router>
         <HeadSection />
         <Routes>
-          <Route path="/" />
-          <Route path="/Shop" />
-          <Route path="/Contact" />
-          <Route path="/About" />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Shop" element={<ShopPage />} />
+          <Route path="/Contact" element={<ContactPage />} />
+          <Route path="/About" element={<AboutPage />} />
         </Routes>
       </Router>
     </Container>
---
+++ src/components/About_Page/About.jsx
@@ -0,0 +1,4 @@
+function AboutPage() {
+  return <h1>About Page</h1>;
+}
+export default AboutPage;
